8502d925572fbcb5a91538c2aeda152cbfb283ee
Daivison-Morais <daivison13pinheiro@gmail.com>
2023-10-04 14:51:57 -0300

    feat: backend em funcionamento

--- src/app.js
+++ src/app.js
@@ -8,4 +8,6 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.use(routes)
+
 module.exports = app;
--- src/controllers/prompt-controller.js
+++ src/controllers/prompt-controller.js
@@ -1,24 +1,35 @@
-const openai = require("../config/openai");
-const inputPrompt = require("../models/input-prompt")
+const { default: OpenAI } = require("openai");
 
 module.exports = {
     async sendText (req, res){
-        const openaiAPI = openai.configuration();
-        const inputModel = new inputPrompt(req.body)
+
         try {
-            const response = await openaiAPI.createCompletion(
-                openai.textCompletion(inputModel)
+            const openai = new OpenAI({
+                apiKey: process.env.OPENAI_API_KEY,
+            });
+
+            const response = await openai.completions.create(
+                {
+                    model: "text-davinci-003",
+                    prompt: req.body.prompt, 
+                    temperature: 0,
+                    max_tokens: 4000,
+                    top_p: 1,
+                    frequency_penalty: 0.60,
+                    presence_penalty: 0.60,
+                  }
             )
 
             return res.status(200).json({
                 sucess: true,
-                data: response.data.choices[0].text
+                data: response.choices[0].text
             })
             
         } catch (error) {
+            console.log(error)
             return res.status(400).json({
                 sucess: false,
-                error: error.response ? error.response : "there was an inssue on the server"
+                error: error.response ? error.response.data : "there was an inssue on the server"
             })
         }
 
